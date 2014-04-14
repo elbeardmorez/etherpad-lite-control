261924640db8d9e02afd3ea5df9860797a03c0db
Pete Beardmore <pete.beardmore@msn.com>
2014-04-14 17:43:31 +0100

    [sup] code cleanup, whitespace changes

--- index.php
+++ index.php
@@ -30,59 +30,58 @@ function getFunctionName($s) {
       }
     ?>
   </div>
+
   <div id="content-outer" style="position: absolute; top: 75px; right: 135px; bottom: 50px; left: 5px; min-width: 700px; overflow: hidden;">
   <div id="content-inner" style="position: relative; height: 100%; width: 95%; margin: auto;">
 
 
     <div style="height: 200px; width: 100%;">
-
-    <div style="float: left; position: relative; height: 100%; width: 175px;">
-      <div style="position: absolute; top: 5px; bottom: 5px; left: 5px; right: 5px;">
-      <form id="etherpad-lite">
-          <p style="margin-top: 8px; margin-bottom: 5px;"><b>settings</b></p>
-          <div style="display: block">
-            <p style="margin-top: 5px; margin-bottom: 2px;">server:</p>
-            <input id="epc_server" type=text onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%;">
-          </div>
-          <div style="display: block">
-            <p style="margin-top: 5px; margin-bottom: 2px;">port:</p>
-            <input id="epc_port" type=text size=25 onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%;">
-          </div>
-          <div style="display: block">
-            <p style="margin-top: 5px; margin-bottom: 2px;">base path:</p>
-            <input id="epc_basepath" type=text size=25 onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%; opacity: 0.5;" disabled>
-          </div>
-          <div style="display: block">
-            <p style="margin-top: 5px; margin-bottom: 2px;">api key path:</p>
-            <input id="epc_apikeypath" type=text size=25 onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%;">
-          </div>
-      </form>
-      </div>
-    </div>
-    <div style="float: right; position: relative; height: 100%; width: 30%; width: 175px;">
-      <div style="position: absolute; top: 5px; bottom: 5px; left: 5px; right: 5px;">
-        <p style="margin-top: 8px; margin-bottom: 5px;"><b>info</b></p>
-        <div id="epInfo-outer" style="position: absolute; top: 35px; bottom: 0px; width: 100%; overflow: auto;">
-        <div id="epInfo-inner" style="position: relative;">
-        </div>
+      <div style="float: left; position: relative; height: 100%; width: 175px;">
+        <div style="position: absolute; top: 5px; bottom: 5px; left: 5px; right: 5px;">
+        <form id="etherpad-lite">
+            <p style="margin-top: 8px; margin-bottom: 5px;"><b>settings</b></p>
+            <div style="display: block">
+              <p style="margin-top: 5px; margin-bottom: 2px;">server:</p>
+              <input id="epc_server" type=text onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%;">
+            </div>
+            <div style="display: block">
+              <p style="margin-top: 5px; margin-bottom: 2px;">port:</p>
+              <input id="epc_port" type=text size=25 onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%;">
+            </div>
+            <div style="display: block">
+              <p style="margin-top: 5px; margin-bottom: 2px;">base path:</p>
+              <input id="epc_basepath" type=text size=25 onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%; opacity: 0.5;" disabled>
+            </div>
+            <div style="display: block">
+              <p style="margin-top: 5px; margin-bottom: 2px;">api key path:</p>
+              <input id="epc_apikeypath" type=text size=25 onchange="if (this.value.length > 0) setCookie(this.id, this.value);" style="width: 100%;">
+            </div>
+        </form>
         </div>
       </div>
-    </div>
-
-    <div style="position: relative; height: 100%; min-width: 200px; margin: 0px 175px;">
-      <div style="position: absolute; top: 5px; bottom: 5px; left: 5px; right: 5px;">
-        <p style="margin-top: 8px; margin-bottom: 5px;"><b>status</b></p>
-        <div id="epStatus-outer" style="position: absolute; top: 35px; bottom: 0px; width: 100%; overflow: auto;">
-        <div id="epStatus-inner" style="position: relative;">
+      <div style="float: right; position: relative; height: 100%; width: 30%; width: 175px;">
+        <div style="position: absolute; top: 5px; bottom: 5px; left: 5px; right: 5px;">
+          <p style="margin-top: 8px; margin-bottom: 5px;"><b>info</b></p>
+          <div id="epInfo-outer" style="position: absolute; top: 35px; bottom: 0px; width: 100%; overflow: auto;">
+          <div id="epInfo-inner" style="position: relative;">
+          </div>
+          </div>
         </div>
+      </div>
+
+      <div style="position: relative; height: 100%; min-width: 200px; margin: 0px 175px;">
+        <div style="position: absolute; top: 5px; bottom: 5px; left: 5px; right: 5px;">
+          <p style="margin-top: 8px; margin-bottom: 5px;"><b>status</b></p>
+          <div id="epStatus-outer" style="position: absolute; top: 35px; bottom: 0px; width: 100%; overflow: auto;">
+          <div id="epStatus-inner" style="position: relative;">
+          </div>
+          </div>
         </div>
       </div>
     </div>
 
-    </div>   
     <div style="float: none"></div>
-  <!--
-  -->
+
     <div style="position: relative; height: 225px; width: 100%;">
       <div style="display: block; float: left; width: 30%; min-width: 150px; margin: auto;">
         <p id="epGroupsTitle" style="margin-bottom: 5px; font-weight: bold;">groups</p>
@@ -108,11 +107,11 @@ function getFunctionName($s) {
 
   </div>
   </div>
+
   <div id="footer" style="position: absolute; bottom: 0px; height: 50px; width: 100%;">
   </div>
 
   <div id="popup-background" class="popup-background" style="position: absolute; height: 100%; width: 100%;"></div>
-
   <div id="popup-outer" class="popup-outer" style="position: absolute; height: 100%; width: 100%;">
   <div id="popup-inner" class="popup-inner" style="position: relative; top: 50%; left: 50%; width: 500px; margin-top: -200px; margin-left: -250px;">
     <h2 id=popupTitle></h2>
